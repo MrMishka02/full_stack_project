311b77ae05e95d0433c9ecbaa7cd59312b52b0e9
Mikheil Gogia <m.gogia@hotmail.com>
2023-10-30 10:22:47 +0400

    docs: added bcrypt library, added msg in validation

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import jwt from "bcrypt";
 import mongoose from "mongoose";
 import { validationResult } from "express-validator";
 import { registerValidation } from "./validations/auth.js";
+import UserModel from "./models/User.js";
 
 mongoose
   .connect(
@@ -15,11 +17,21 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/auth/register", registerValidation, (req, res) => {
+app.post("/auth/register", registerValidation, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
   }
+
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash: req.body.passwordHash,
+  });
 
   res.json({
     success: true,
--- validations/auth.js
+++ validations/auth.js
@@ -1,8 +1,8 @@
 import { body } from "express-validator";
 
 export const registerValidation = [
-  body("email").isEmail(),
-  body("password").isLength({ min: 5 }),
-  body("fullName").isLength({ min: 3 }),
-  body("avatar").optional().isURL(),
+  body("email", "Incorect email address!").isEmail(),
+  body("password", "Password must have min 5 letters!").isLength({ min: 5 }),
+  body("fullName", "Input the name!").isLength({ min: 3 }),
+  body("avatarUrl", "Incorrect url!").optional().isURL(),
 ];
